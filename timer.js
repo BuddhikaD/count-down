3b394a169158a0616254fdb6ea3491cba312a823
Buddhika Dananjaya <buddhikadananjaya55@gmail.com>
2023-05-22 08:38:29 +0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -5,6 +5,7 @@ const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
 const year = document.getElementById('year');
 const loading = document.getElementById('loading');
+const congratulations = document.getElementById('congratulations');
 
 const currentYear = new Date().getFullYear();
 
@@ -15,6 +16,8 @@ year.innerText = currentYear;
 
 // Update countdown time
 function updateCountdown() {
+
+
   const currentTime = new Date();
   const diff = newYearTime - currentTime;
 
@@ -23,11 +26,18 @@ function updateCountdown() {
   const m = Math.floor(diff / 1000 / 60) % 60;
   const s = Math.floor(diff / 1000) % 60;
 
-  // Add values to DOM
-  days.innerHTML = d;
-  hours.innerHTML = h < 10 ? '0' + h : h;
-  minutes.innerHTML = m < 10 ? '0' + m : m;
-  seconds.innerHTML = s < 10 ? '0' + s : s;
+  if(d<0 && h<0 && m<0 && s<1){
+    stop(timer)
+    countdown.style.display = 'none';
+    congratulations.style.display = 'inline-block';
+  }else{
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+  }
+
+
 }
 
 // Show spinner before countdown
@@ -36,5 +46,12 @@ setTimeout(() => {
   countdown.style.display = 'flex';
 }, 1000);
 
+function stop(timer) {
+  if (timer) {
+      clearTimeout(timer);
+      timer = 0;
+  }
+}
+
 // Run every second
-setInterval(updateCountdown, 1000);
+const timer = setInterval(updateCountdown, 1000);
